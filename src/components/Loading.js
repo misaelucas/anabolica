0e6df45a1cbcb7259f1b1996e1811daaa975a456
misael <misaelucas@gmail.com>
2023-06-22 00:41:32 -0300

    new loading component.

---
+++ src/components/Loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { PulseLoader } from "react-spinners";
+import '../assets/spinner.css';
+
+export const Loading = () => {
+  return (
+    <div className="flex spinnerz justify-center">
+
+    </div>
+  );
+};
